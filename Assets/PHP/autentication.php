3deee8b7670ab6e37e4e410489de327bc6ef1881
Nicolò Pratelli <pratelli1991@gmail.com>
2018-11-16 17:14:44 +0100

    try not work auth

--- Assets/PHP/autentication.php
+++ Assets/PHP/autentication.php
@@ -80,6 +80,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     include '../Api/extractPersonInformation.php';
     header("location: welcome.php");
   }else {
+    echo $count;
     //header("location: no_login.php?error=inv_user_password");
   }
 }
